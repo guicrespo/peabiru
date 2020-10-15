e64c1bd9dd2bcc38cb72280ac53920f3a2003f27
guicgs <etohlif@protonmail.com>
2020-10-15 14:09:24 -0300

    CC

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -22,13 +22,33 @@ const Contact = () => {
       <section className="contact-section">
         <form className="contact-form" onSubmit={sendEmail}>
           <label htmlFor="name">Nome</label>
-          <input id="name" type="text" required onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, name: value })} />
+          <input
+            id="name"
+            type="text"
+            required
+            onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, name: value })}
+          />
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" required onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, email: value })} />
+          <input
+            id="email"
+            type="email"
+            required
+            onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, email: value })}
+          />
           <label htmlFor="subject">Assunto</label>
-          <input id="subject" type="text" required onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, subject: value })} />
+          <input
+            id="subject"
+            type="text"
+            required
+            onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, subject: value })}
+          />
           <label htmlFor="message">Mensagem</label>
-          <textarea id="message" rows="10" required onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, message: value })} />
+          <textarea
+            id="message"
+            rows="10"
+            required
+            onChange={({ target: { value } }) => setSenderInfo({ ...senderInfo, message: value })}
+          />
           <button type="submit">Enviar</button>
         </form>
         <section className="other-contacts">
