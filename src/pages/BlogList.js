dec6b52bc5bfc9ddecacd7d6e8a96c2c8a100639
guicgs <etohlif@protonmail.com>
2020-10-20 12:28:52 -0300

    renderizando lista de posts

---
+++ src/pages/BlogList.js
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import dompurify from 'dompurify';
+import { AppContext } from '../context';
+
+const sanitizer = dompurify.sanitize;
+
+const BlogList = () => {
+  const { setUnderscore } = useContext(AppContext);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://techcrunch.com/wp-json/wp/v2/posts').then((result) => setPosts(result.data));
+    setUnderscore('blog');
+  }, []);
+
+  return (
+    <section className="content-body">
+      <h2>Blog</h2>
+      <section>
+        {posts.map((post) => (
+          <article key={post.id}>
+            <Link to={`/blog/${post.slug}`}>
+              <div className="card" key={post.id}>
+                <div className="card-content">
+                  <h3>{post.title.rendered}</h3>
+                  <div dangerouslySetInnerHTML={{ __html: sanitizer(post.excerpt.rendered) }} />
+                </div>
+              </div>
+            </Link>
+          </article>
+        ))}
+      </section>
+    </section>
+  );
+};
+
+export default BlogList;
