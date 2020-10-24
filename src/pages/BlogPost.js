b9389f59e252361f81e611814d1895675774f6c9
guicgs <etohlif@protonmail.com>
2020-10-24 12:33:44 -0300

    CC

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const BlogPost = () => {
-  return (
-    <section className="content-body">
-      <h2>Blog</h2>
-    </section>
-  );
-};
+const BlogPost = () => (
+  <section className="content-body">
+    <h2>Blog</h2>
+  </section>
+);
 
 export default BlogPost;
