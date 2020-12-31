4eba5170f1611c311600be47e6605169829e48f9
guicgs <etohlif@protonmail.com>
2020-12-31 10:11:11 -0300

    adicionando configuração graphcms para blog

--- src/pages/BlogList.js
+++ src/pages/BlogList.js
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import dompurify from 'dompurify';
+import { request } from 'graphql-request';
+// import dompurify from 'dompurify';
 import { AppContext } from '../context';
 
-const sanitizer = dompurify.sanitize;
+// const sanitizer = dompurify.sanitize;
 
 const BlogList = () => {
   const { setUnderscore } = useContext(AppContext);
@@ -12,26 +12,51 @@ const BlogList = () => {
 
   useEffect(() => {
     setUnderscore('blog');
-    axios.get('https://techcrunch.com/wp-json/wp/v2/posts').then((result) => setPosts(result.data));
+    const fetchPosts = async () => {
+      const { blogPost } = await request(
+        process.env.REACT_APP_GRAPHCMS_URI,
+        `
+          query {
+            blogPost {
+              id
+              slug
+              title
+              updatedAt
+              author
+              description
+              body {
+                html
+              }
+            }
+          }
+        `,
+      );
+      setPosts(blogPost);
+    };
+
+    fetchPosts();
   }, []);
 
   return (
     <section className="content-body">
       <h2>Blog</h2>
       <section>
-        {posts.map((post) => (
-          <article key={post.id}>
-            <Link to={`/blog/${post.slug}`}>
-              <div className="card" key={post.id}>
-                <div className="card-content">
-                  <h3>{post.title.rendered}</h3>
-                  {/* eslint-disable-next-line react/no-danger */}
-                  <div dangerouslySetInnerHTML={{ __html: sanitizer(post.excerpt.rendered) }} />
+        {!posts ? <div>carregando</div>
+          : posts.map((post) => (
+            <article key={post.id}>
+              <Link to={`/blog/${post.slug || ''}`}>
+                <div className="card" key={post.id}>
+                  <div className="card-content">
+                    <h3>{post.title}</h3>
+                    <h4>{post.author}</h4>
+                    <p>{post.description}</p>
+                    {/* eslint-disable-next-line react/no-danger */}
+                    {/* <div dangerouslySetInnerHTML={{ __html: sanitizer(post.body.html) }} /> */}
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </article>
-        ))}
+              </Link>
+            </article>
+          ))}
       </section>
     </section>
   );
